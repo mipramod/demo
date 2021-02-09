6f7b028d5668896dabd171b1be03bf2eb28921cc
mipramod <78783306+mipramod@users.noreply.github.com>
2021-02-09 12:16:54 +0530

    changed name of variable

--- add.sh
+++ add.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-read -p "Enter first value" a
+read -p "Enter the value of num1 " num1
 read -p "Enter second value" b
 
-sum=$(( $a + $b ))
+sum=$(( $num1 + $b ))
 
 echo "sum= $sum"
